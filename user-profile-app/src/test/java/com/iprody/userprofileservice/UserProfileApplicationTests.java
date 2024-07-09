ce1883940fc6f755712bc4a08f1244e580cbdb0f
Rustam Abdusamatov <110726031+RustamAbdusamatov4848@users.noreply.github.com>
2024-07-09 18:45:02 +0300

    Created basic project structure (#1)
    * Created basic project structure
    
    * Add a separate application.properties file for tests
    
    * Change project structure.
    Add model user-profile-app
    Fix pom files
    
    * Mock delete module
    
    * - FINAL VERSION
    
    Change project structure.
    Add model user-profile-app
    Fix pom files
    
    ---------------------------------------------------/
    The application runs without errors
    ---------------------------------------------------/

---
+++ user-profile-app/src/test/java/com/iprody/userprofileservice/UserProfileApplicationTests.java
@@ -0,0 +1,12 @@
+package com.iprody.userprofileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserProfileApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
+
