c08cadab2c2a87db45274f27d29ed2ab50be1c80
Rustam Abdusamatov <110726031+RustamAbdusamatov4848@users.noreply.github.com>
2024-07-31 11:58:58 +0300

    Add user domain (#25)
    * 1. Liquibase configured
    2. Tables structure defined in liquibase changelog
    3. JPA entity for User defined
    4. Corresponding JPA repository declared
    5. Test data prepared for being populated
    6. User service covered with integration tests
    
    * Create local database
    
    * Fix
    
    * Fix
    
    * 1. Change Liquibase package structure
    2. Add SQL for creating users table
    3. Remove Service
    
    * Added some tiny changes
    
    * 1. Fix changelog sequence error
    2. Add changeSet initial tag
    3. Change db name in docker-compose.yaml
    
    * Add some changes according review
    
    * Add some changes according review
    
    * Add some changes according review

--- user-profile-app/src/test/java/com/iprody/userprofileservice/UserProfileApplicationTests.java
+++ user-profile-app/src/test/java/com/iprody/userprofileservice/UserProfileApplicationTests.java
@@ -1,12 +1,11 @@
 package com.iprody.userprofileservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class UserProfileApplicationTests {
-    @Test
-    void contextLoads() {
-    }
+
 }
 
