c3deb64a78fb839c1798bf75256b7d6fed8c66aa
Rustam Abdusamatov <110726031+RustamAbdusamatov4848@users.noreply.github.com>
2024-08-01 21:08:30 +0300

    Add service layer and tests (#26)
    * 1. Add service layer
    2. Add tests
     2.1 Add integration test for UserService.java
     2.2 Add mapper test
     2.3 Add validation test
    
    * Fix tests
    
    * Add mode for @Mapper
    
    * Fix service layer according review

---
+++ user-profile-app/src/main/java/com/iprody/userprofileservice/dto/UserDto.java
@@ -0,0 +1,21 @@
+package com.iprody.userprofileservice.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    @Size(max = 50)
+    private String firstName;
+    @Size(max = 50)
+    private String lastName;
+    @Email
+    private String email;
+}
