3bcc006ed4711ac382cda55d12f3b2314500c96e
Rustam Abdusamatov <110726031+RustamAbdusamatov4848@users.noreply.github.com>
2024-08-28 08:38:59 +0300

    Add Open Api (#34)
    * Add Open Api
    
    * Add Open Api
    
    * Fix test controller
    
    Add handler for invalid dto's
    fix open-api.yaml
    fix controller
    
    Add fix for Open Api
    
    * Fix test controller
    
    * Fix test controller
    
    * Added a meaningful message in case the entity is not found
    
    * Add spring actuator config

--- user-profile-app/src/main/java/com/iprody/userprofileservice/controllers/UserController.java
+++ user-profile-app/src/main/java/com/iprody/userprofileservice/controllers/UserController.java
@@ -4,103 +4,71 @@ import com.iprody.userprofileservice.dto.UserContactDto;
 import com.iprody.userprofileservice.dto.UserDto;
 import com.iprody.userprofileservice.services.UserContactService;
 import com.iprody.userprofileservice.services.UserService;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.MessageSourceResolvable;
+import org.openapitools.api.V1UserApi;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
 
 @RestController
-@RequestMapping("/v1/users")
 @RequiredArgsConstructor
-public class UserController {
+public class UserController implements V1UserApi {
 
     private final UserService userService;
     private final UserContactService userContactService;
 
-    @GetMapping("/{id}")
+    @Override
     public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long userId) {
         return userService.findUserById(userId)
                 .map(user -> new ResponseEntity<>(user, HttpStatus.OK))
-                .orElseThrow(() -> new ResponseStatusException(
-                        HttpStatus.NOT_FOUND, "User with ID: " + userId + " not found"));
+                .orElseThrow(() -> new EntityNotFoundException(
+                        "User with user ID: " + userId + " not found"));
     }
 
-    @PostMapping
-    public ResponseEntity<?> createUser(@Valid @RequestBody UserDto userDto,
-                                             BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return handleValidationErrors(bindingResult);
-        }
-
+    @Override
+    public ResponseEntity<Object> createUser(@Valid @RequestBody UserDto userDto) {
         UserDto createdUserDto = userService.createUser(userDto);
         return new ResponseEntity<>(createdUserDto, HttpStatus.CREATED);
     }
 
-    @PutMapping()
-    public ResponseEntity<?> updateUser(@Valid @RequestBody UserDto userDto,
-                                             BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return handleValidationErrors(bindingResult);
-        }
-
+    @Override
+    public ResponseEntity<Object> updateUser(@Valid @RequestBody UserDto userDto) {
         userService.updateUser(userDto);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @DeleteMapping("/{id}")
+    @Override
     public ResponseEntity<Void> deleteUserById(@PathVariable("id") Long userId) {
         userService.deleteUserById(userId);
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/contacts/{id}")
+    @Override
     public ResponseEntity<UserContactDto> getUserContactByContactId(@PathVariable("id") Long userContactId) {
         return userContactService.findUserContactsById(userContactId)
                 .map(contact -> new ResponseEntity<>(contact, HttpStatus.OK))
-                .orElseThrow(() -> new ResponseStatusException(
-                        HttpStatus.NOT_FOUND, "User contact with ID: " + userContactId + " not found"));
+                .orElseThrow(() -> new EntityNotFoundException(
+                        "User contact with user contact ID: " + userContactId + " not found"));
     }
 
-    @GetMapping("/{id}/contacts")
+    @Override
     public ResponseEntity<UserContactDto> getUserContactByUserId(@PathVariable("id") Long userId) {
         return userContactService.findUserContactByUserId(userId)
                 .map(contact -> new ResponseEntity<>(contact, HttpStatus.OK))
-                .orElseThrow(() -> new ResponseStatusException(
-                        HttpStatus.NOT_FOUND, "User contact with user ID: " + userId + " not found"));
+                .orElseThrow(() -> new EntityNotFoundException(
+                        "User contact with user ID: " + userId + " not found"));
     }
 
-    @PutMapping("/{id}/contacts")
-    public ResponseEntity<?> updateUserContactByUserId(
+    @Override
+    public ResponseEntity<Object> updateUserContactByUserId(
             @PathVariable("id") Long userId,
-            @Valid @RequestBody UserContactDto userContactDto,
-            BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return handleValidationErrors(bindingResult);
-        }
+            @Valid @RequestBody UserContactDto userContactDto) {
 
         userContactService.updateContacts(userContactDto, userId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-    private ResponseEntity<Object> handleValidationErrors(BindingResult bindingResult) {
-        List<String> errors = bindingResult
-                .getAllErrors()
-                .stream()
-                .map(MessageSourceResolvable::getDefaultMessage)
-                .toList();
-        return ResponseEntity.badRequest().body(errors);
     }
 }
