f05aa1192e93477073927102efddcab5dc8dd6c7
Rustam Abdusamatov <110726031+RustamAbdusamatov4848@users.noreply.github.com>
2024-09-05 20:11:43 +0300

    Correct OpenApi and business layer changes (#47)
    * Add some changes
    
    * Add some changes
    
    * Add some changes
    
    * Add some changes
    
    * Add some changes

--- user-profile-app/src/main/java/com/iprody/userprofileservice/services/impl/UserServiceImpl.java
+++ user-profile-app/src/main/java/com/iprody/userprofileservice/services/impl/UserServiceImpl.java
@@ -2,6 +2,8 @@ package com.iprody.userprofileservice.services.impl;
 
 import com.iprody.userprofileservice.dto.UserDto;
 import com.iprody.userprofileservice.models.User;
+import com.iprody.userprofileservice.models.UserContact;
+import com.iprody.userprofileservice.repositories.UserContactRepository;
 import com.iprody.userprofileservice.repositories.UserRepository;
 import com.iprody.userprofileservice.services.UserService;
 import com.iprody.userprofileservice.utils.UserMapper;
@@ -20,6 +22,7 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final UserContactRepository userContactRepository;
 
 
     public Optional<UserDto> findUserById(Long userDd) {
@@ -30,9 +33,17 @@ public class UserServiceImpl implements UserService {
 
     @Transactional
     public UserDto createUser(UserDto userDTO) {
-        User savedUser = userRepository
-                .save(userMapper.userDtoToUser(userDTO));
+        User user = userMapper.userDtoToUser(userDTO);
+
+        if (user.getUserContact() != null && user.getUserContact().getId() != null) {
+            UserContact existingContact = userContactRepository
+                    .getReferenceById(user.getUserContact().getId());
+            user.setUserContact(existingContact);
+        }
+
+        User savedUser = userRepository.save(user);
         log.info("Saved user with ID: {}", savedUser.getId());
+
         return userMapper.userToUserDto(savedUser);
     }
 
@@ -41,7 +52,6 @@ public class UserServiceImpl implements UserService {
         userRepository.findById(userDTO.getId())
                 .map(user -> userMapper.updateUserFromDto(userDTO, user))
                 .map(userRepository::save);
-
         log.info("Update user with ID: {}", userDTO.getId());
     }
 
